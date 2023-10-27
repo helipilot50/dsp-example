d44c962c8fe93f0d851dfb113528a7261c1c105f
p.milne <p.milne@criteo.com>
2023-10-27 12:24:05 -0500

    frontend build script

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 echo "Building frontend..."
-mkdir -p publish
-echo "created publish folder"
+
+# not-dsp-graphql
+echo "building not-dsp-graphql"
+cd not-dsp-graphql
+yarn build
+cd ..
 
 # React
-mkdir -p publish/react
+echo "building react"
 cd react
 yarn build
 cd ..
-echo "built react"
-cp -r react/build/* publish/react
-echo "copied react to publish"
 
-# Angular
-mkdir -p publish/angular
-cd angular
-yarn build
-cd ..
-echo "built angular"
-cp -r angular/dist/not-dsp-angular/* publish/angular
-echo "copied angular to publish"
+# # Angular
+# mkdir -p publish/angular
+# cd angular
+# yarn build
+# cd ..
+# echo "built angular"
+# cp -r angular/dist/not-dsp-angular/* publish/angular
+# echo "copied angular to publish"
