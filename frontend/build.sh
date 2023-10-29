55cea12f204c5b772c3f90706895028530ecaa05
p.milne <p.milne@criteo.com>
2023-10-29 16:13:57 -0500

    react build

--- frontend/build.sh
+++ frontend/build.sh
@@ -4,20 +4,19 @@ echo "Building frontend..."
 # not-dsp-graphql
 echo "building not-dsp-graphql"
 cd not-dsp-graphql
+yarn install
 yarn build
 cd ..
 
 # React
 echo "building react"
 cd react
+yarn install
 yarn build
 cd ..
 
 # # Angular
-# mkdir -p publish/angular
 # cd angular
+# yarn install
 # yarn build
 # cd ..
-# echo "built angular"
-# cp -r angular/dist/not-dsp-angular/* publish/angular
-# echo "copied angular to publish"
