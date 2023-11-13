ae8d06ec35cf21ba411f91a94819103c1a41b12f
p.milne <p.milne@criteo.com>
2023-11-13 10:27:38 +0100

    removed old service added yarn lock files

--- backend/dsp-service/dist/resolvers/resolverTools.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.topLevelFieldsFromQuery = void 0;
-function topLevelFieldsFromQuery(info) {
-    // context.logger.debug(`topLevelFieldsFromQuery ${JSON.stringify(info, null, 2)}`);
-    return info.fieldNodes[0].selectionSet?.selections.map((sel) => sel.name.value).filter((name) => name !== '__typename');
-}
-exports.topLevelFieldsFromQuery = topLevelFieldsFromQuery;
